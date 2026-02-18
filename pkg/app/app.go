d11126464af0c57672da3d5490397e948cac9d4b
cylonchau <cylonchau@outlook.com>
2026-02-18 17:30:45 +0800

    fix: resolve merge conflicts and add default Makefile parameters

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -6,13 +6,15 @@ import (
 	"github.com/cylonchau/hermes/pkg/app/router"
 	"github.com/cylonchau/hermes/pkg/config"
 	"github.com/cylonchau/hermes/pkg/logger"
+
+	"github.com/gin-gonic/gin"
 )
 
 // NewHTTPSever 启动 HTTP 管理服务
 func NewHTTPSever() error {
 	cfg := config.Get()
 	addr := fmt.Sprintf(":%d", cfg.Server.Port)
-	logger.Info("Hermes HTTP server listening on %s\n", addr)
+	logger.Info("Hermes HTTP server listening", logger.String("addr", addr))
 
 	// Initialize Gin
 	engine := gin.Default()
